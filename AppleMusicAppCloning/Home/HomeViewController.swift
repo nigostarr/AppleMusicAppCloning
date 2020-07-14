a1881069c62ac2d27d3595cbc792126fe0834067
Nigostarr <wlckdgus5833@naver.com>
2020-07-15 01:35:04 +0900

    #3. Transmit information of collectionViewCell to Storyboard and Build TrackManager.

--- AppleMusicAppCloning/Home/HomeViewController.swift
+++ AppleMusicAppCloning/Home/HomeViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class HomeViewController: UIViewController {
     // TODO: 트랙관리 객체 추가
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -21,15 +20,17 @@ extension HomeViewController: UICollectionViewDataSource {
     // 몇개 표시 할까?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO: 트랙매니저에서 트랙갯수 가져오기
-        return 0
+        return 10
     }
     
     // 셀 어떻게 표시 할까?
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // TODO: 셀 구성하기
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollectionViewCell", for: indexPath) as? TrackCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+            return cell
     }
-    
     // 헤더뷰 어떻게 표시할까?
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
@@ -54,6 +55,13 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         // 20 - card(width) - 20 - card(width) - 20
         // TODO: 셀사이즈 구하기
-        return CGSize.zero
+        
+        // itemSpacing = 카드와 카드간의 거리
+        let itemSpacing : CGFloat = 20
+        let margin: CGFloat = 20
+        let width = (collectionView.bounds.width - itemSpacing - margin * 2)/2
+        let height = width + 60
+        return CGSize(width: width, height: height)
+        
     }
 }
